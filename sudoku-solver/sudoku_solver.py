ddc44af22b002d8f45f29904516e945522e01ead
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-04-18 23:48:53 +0100

    feat:sudoku solver

---
+++ sudoku-solver/sudoku_solver.py
@@ -0,0 +1,21 @@
+class Board:
+    def __init__(self, board):
+        self.board = board
+
+    def find_empty_cell(self):
+        for row, contents in enumerate(self.board):
+            try:
+                col = contents.index(0)
+                return row, col
+            except ValueError:
+                pass
+        return None
+
+    def valid_in_row(self, row, num):
+        return num not in self.board[row]
+
+    def valid_in_col(self, col, num):
+        return all(self.board[row][col] != num for row in range(9))
+    def valid_in_square(self, row, col, num):
+        row_start = (row // 3) * 3
+        col_start = (col // 3) * 3
