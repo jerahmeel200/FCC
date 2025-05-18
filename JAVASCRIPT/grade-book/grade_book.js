0d3a7e5acc6f6f01d23ef9e955b6d9ccc608add6
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-05-19 00:21:28 +0100

    feat: has passing grade function

--- JAVASCRIPT/grade-book/grade_book.js
+++ JAVASCRIPT/grade-book/grade_book.js
@@ -26,3 +26,20 @@ function getGrade(score) {
     return "F";
   }
 }
+
+
+// function hasPassingGrade(score) {
+//   if(getGrade(score) === "F"){
+//     return false
+//   }else{
+//     return true
+//   }
+// }
+
+function hasPassingGrade(score) {
+  return getGrade(score) !== "F";
+}
+
+console.log(hasPassingGrade(100));
+console.log(hasPassingGrade(53));
+console.log(hasPassingGrade(87));
