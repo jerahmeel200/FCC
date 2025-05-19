502468caf41c2e352482f2045c7331ed4b5f89a2
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-05-19 01:42:56 +0100

    feat: student messsage function

--- JAVASCRIPT/grade-book/grade_book.js
+++ JAVASCRIPT/grade-book/grade_book.js
@@ -43,3 +43,13 @@ function hasPassingGrade(score) {
 console.log(hasPassingGrade(100));
 console.log(hasPassingGrade(53));
 console.log(hasPassingGrade(87));
+
+
+function studentMsg(totalScores, studentScore) {
+  const average = getAverage(totalScores);
+  const grade = getGrade(studentScore);
+  const result = hasPassingGrade(studentScore);
+  return `Class average: ${average}. Your grade: ${grade}. You ${result ? "passed" : "failed"} the course.`;
+}
+
+console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
